a46a6716d24434b91c9b64b9602245016423e324
lucianolo <luciano.ursache@gmail.com>
2016-04-16 12:37:23 +0000

    Updated FrontEnd for the new authentication system
    Added SimpleForm,
    Updated several forms to bootstrap and simpleform,
    Added Login - Logout - Register and Profile to our nava,
    Added the collection select to our status form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          
+         
+  has_many :statuses
+         
+  def full_name
+    first_name+" "+last_name
+  end
  
 end
   
--- db/migrate/20160415205455_devise_create_users.rb
+++ db/migrate/20160415205455_devise_create_users.rb
@@ -2,9 +2,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def change
     create_table(:users) do |t|
       ## Database authenticatable
-      t.string :first_name
-      t.string :last_name
-      t.string :profile_name
+      t.string :first_name, null: false, default: ""
+      t.string :last_name, null: false, default: ""
+      t.string :profile_name, null: failse, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
