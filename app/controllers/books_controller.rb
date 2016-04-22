02eb2f24f8ca5f2b2b0ed9e3fd5e78db58b9a811
lucianolo <luciano.ursache@gmail.com>
2016-04-22 13:01:29 +0000

    Add animation to books on index and profile, minor design changes, bug fixes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 
 class BooksController < ApplicationController
+  before_filter :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
