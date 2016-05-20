9370542583b9db9215efbfa99e1d86b445a92ba5
lucianolo <luciano.ursache@gmail.com>
2016-05-20 11:39:08 +0000

    Add Conversation and Message models

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160518104658) do
+ActiveRecord::Schema.define(version: 20160520113741) do
 
   create_table "books", force: :cascade do |t|
     t.integer  "user_id"
@@ -39,6 +39,25 @@ ActiveRecord::Schema.define(version: 20160518104658) do
 
   add_index "comments", ["book_id"], name: "index_comments_on_book_id"
   add_index "comments", ["user_id"], name: "index_comments_on_user_id"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer  "sender_id"
+    t.integer  "recipient_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "conversation_id"
+    t.integer  "user_id"
+    t.boolean  "read",            default: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id"
+  add_index "messages", ["user_id"], name: "index_messages_on_user_id"
 
   create_table "statuses", force: :cascade do |t|
     t.text     "content"
